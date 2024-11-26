8c87852711e4566b8763fba5b153ae926315f7f1
daihaoyu <1610882575@qq.com>
2024-11-26 14:33:23 +0800

    _cf

---
+++ demo/src/main/java/redis/t1.java
@@ -0,0 +1,19 @@
+package redis;
+
+import redis.clients.jedis.Jedis;
+
+public class t1 {
+
+    public static void main(String[] args) {
+        //连接本地的 Redis 服务
+        Jedis jedis = new Jedis("192.168.161.185",6379);
+        // 如果 Redis 服务设置了密码，需要下面这行，没有就不需要
+
+        System.out.println("连接成功");
+        //查看服务是否运行
+//
+//        System.out.println(jedis.set());
+//        System.out.println("redis 存储的字符串为: "+ jedis.get("runoobkey"));
+        System.out.println("服务正在运行: "+jedis.ping());
+    }
+}
